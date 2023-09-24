d6d880f11bc70b850bc5ea9f8705c76ed83e0bac
caio <caio.buono8@gmail.com>
2023-09-24 13:34:56 -0300

    Simulando o banco de dados (DB)

--- app/Controller/Pages/Page.php
+++ app/Controller/Pages/Page.php
@@ -5,6 +5,23 @@ namespace App\Controller\Pages;
 use \App\Utils\View;
 
 class Page {
+  
+  /**
+   * Método responsável por renderizar o topo da página
+   * @return string
+   */
+  private static function getHeader() {
+    return View::render('pages/header');
+  }
+  
+
+  /**
+   * Método responsável por renderizar o rodapé da página
+   * @return string
+   */
+  private static function getFooter() {
+    return View::render('pages/footer');
+  }
 
 
   /**
@@ -13,8 +30,10 @@ class Page {
    */
   public static function getPage($title, $content) {
     return View::render('pages/page', [
-        'title' => $title,
-        'content' => $content
+        'title'   => $title,
+        'header'  => self::getHeader(),
+        'content' => $content,
+        'footer'  => self::getFooter()
     ]);
   }
 
---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization {
+
+  /**
+   * ID da organização
+   * @var int
+   */
+  public $id = 1;
+
+  /**
+   * Nome responsável
+   * @var string
+   */
+  public $name = 'Caio';
+
+  /**
+   * Site criador
+   * @var string
+   */
+  public $gitHub = 'https://github.com/CaioBuono';
+
+  /**
+   * Descrição de como funciona
+   * @var string
+   */
+  public $description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, aut! Quam odit accusantium deleniti consectetur culpa sint impedit. Nam ipsum veniam deleniti. Suscipit, et quis! Quasi eaque asperiores officiis cum.';
+
+
+}
